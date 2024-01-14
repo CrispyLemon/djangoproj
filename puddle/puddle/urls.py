4b9ac5c828116088e2dddc3508dceb3164516a7a
CrispyLemon <aryanb349@gmail.com>
2024-01-15 00:03:19 +0530

    commit 3: logging off for now

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from core.views import index, contact
 
 urlpatterns = [
     path('', index, name = 'index'),
     path('admin/', admin.site.urls),
     path('contact/', contact, name="contact")
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
