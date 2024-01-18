60ae075e371fcba1bc37be8def312c8612f3ae97
CrispyLemon <aryanb349@gmail.com>
2024-01-18 19:10:57 +0530

    Revert "commit5 saving progress"
    This reverts commit 84234f3c3358bc80854009a30a28dbce660add13.
    
    accidentally rmrf'd myself

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from item.models import Category, Item
 
 from .forms import SignupForm
@@ -15,17 +15,7 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    if request.method == "POST":
-        form = SignupForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-            return redirect('/login/')
-        
-        else: 
-            form = SignupForm()
-    form = SignupForm() 
+    form = SignupForm()
 
     return render(request, 'core/signup.html', {
         'form':form
