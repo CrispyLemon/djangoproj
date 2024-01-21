d267a97e695c220aa7b2e0a74366732b4cd34cc2
CrispyLemon <aryanb349@gmail.com>
2024-01-21 21:33:00 +0530

    tried to fix category filter in items view

--- djangoproj/puddle/item/views.py
+++ djangoproj/puddle/item/views.py
@@ -7,15 +7,21 @@ from .forms import NewItemForm, EditItemForm
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category_id', 0)
     categories = Category.objects.all()
     items = Item.objects.filter(is_sold = False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
 
     if query:
         items = items.filter(Q(name__icontains = query) | Q(description__icontains = query))
 
     return render(request, 'item/items.html', {
         'items' : items,
-        'query' : query,      
+        'query' : query,
+        'categories':categories,
+        'category_id' : int(category_id),      
     })
 
 def detail(request, pk):
@@ -24,7 +30,7 @@ def detail(request, pk):
 
     return render(request, 'item/detail.html',{
         'item':item,
-        'related_items': related_items
+        'related_items': related_items,
     })
 @login_required
 def new(request):
